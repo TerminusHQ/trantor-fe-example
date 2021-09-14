b05e52c0e3193348c4ba32fc0bfcb1d304525c2e
xuchao <ssaylo@163.com>
2021-09-14 14:53:09 +0800

    fix: render bug

--- src/components/RouterDemo/index.tsx
+++ src/components/RouterDemo/index.tsx
@@ -4,13 +4,14 @@ import MyVueComponent from './RouterDemo.vue'
 // 容器类型
 import { ItemContainer } from '@terminus/nusi-engine'
 
+const Component = VueInReact(MyVueComponent)
+
 export default class extends ItemContainer {
   render() {
   	// vuera 
-    const Component = VueInReact(MyVueComponent)
     return (    
-		    // 将ListContainer 的属性透传到 vue 组件中
-        <Component message='Hello from Vue!' ctx={this.props}/>
+      // 将ListContainer 的属性透传到 vue 组件中
+      <Component message='Hello from Vue!' ctx={this.props}/>
     )
   }
 }
